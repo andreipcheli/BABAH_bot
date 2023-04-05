5c2b2a477334f75b9dbee07444374752eb65d6ed
Andrei Pchelintsev <andreypcheli@gmail.com>
2023-04-05 22:07:44 +0200

    Structure improved

--- song/main.py
+++ song/main.py
@@ -1,13 +1,23 @@
+from spotify.main import get_response_from_spotify, check_response_from_spotify
+from genius.main import get_response_from_genius, check_response_from_genius
+
 def get_song_name():
     pass
 
 
-def create_response():
-    pass
+def create_response(data):
+    unvalid_spotify = get_response_from_spotify(data)
+    valid_spotify = check_response_from_spotify(unvalid_spotify)
 
 
 def main():
-    pass
+    # Отправить сообщение
+    # Получить название трека (get_song_name())
+    # Отправить в create_response
+    # Отправить сообщение из create_response
+    data = get_song_name()
+    create_response(data)
+
 
 if __name__ == '__main__':
     main()
